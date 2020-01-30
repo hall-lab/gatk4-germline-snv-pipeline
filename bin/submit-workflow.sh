1499cefbf32b7f3fd02937fbcd61551c713067f6
indraniel <indraniel@gmail.com>
2020-01-30 08:55:14 +0000

    + adjust script to submit workflow
      - account for the dependency task file
      - update the inputs and workflow wdl

--- bin/submit-workflow.sh
+++ bin/submit-workflow.sh
@@ -6,18 +6,36 @@ set -eo pipefail
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 DATA_DIR=${SOURCE_DIR}/..
 
-WDL=${DATA_DIR}/joint-discovery-gatk4.wdl
-INPUTS=${DATA_DIR}/joint-discovery-gatk4.hg38.wgs.inputs.json
-OPTIONS=${DATA_DIR}/2019.06.14.options.json
+
+WDL=${DATA_DIR}/JointGenotyping.wdl
+INPUTS=${DATA_DIR}/JointGenotyping.hg38.wgs.inputs.json
+OPTIONS=${DATA_DIR}/2020.01.29.options.json
+ZIP=${DATA_DIR}/tasks.zip
+
+if [ -e ${ZIP} ]; then
+    echo "Nuking out old dependency zip: ${ZIP}"
+    rm ${ZIP}
+fi
+
+echo "Creating dependency zip: ${ZIP}"
+cd ${DATA_DIR}; /usr/bin/zip -r ${ZIP} ./tasks
 
 #echo "WDL=${WDL}"
 #echo "INPUTS=${INPUTS}"
 #echo "OPTIONS=${OPTIONS}"
+#echo "ZIP=${ZIP}"
 
 set -o xtrace
 
-curl -v "localhost:8000/api/workflows/v1" -F workflowSource=@${WDL} -F workflowInputs=@${INPUTS} -F workflowOptions=@${OPTIONS} 
+curl -v "localhost:8000/api/workflows/v1" \
+	-F workflowSource=@${WDL} \
+	-F workflowInputs=@${INPUTS} \
+	-F workflowOptions=@${OPTIONS} \
+	-F workflowDependencies=@${ZIP}
 
 set +o xtrace
+
+echo "Nuking out dependency zip: ${ZIP}"
+rm ${ZIP}
 
 # bash bin/submit-workflow.sh 2>&1 | tee logs/client/submit-job.log.$(date "+%Y.%m.%d.%H.%M")
